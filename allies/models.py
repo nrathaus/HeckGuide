32e1df91d43dcb20ff16070a380102e98fe4798c
Noam Rathaus <noamr@ssd-disclosure.com>
2022-12-13 11:03:57 +0200

    Styling

--- allies/models.py
+++ allies/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.db import models
 
+
 class BaseAlly(models.Model):
     username = models.CharField(max_length=255)
     avatar_type = models.IntegerField(null=True)
@@ -24,22 +25,34 @@ class BaseAlly(models.Model):
     biome7_attack_multiplier = models.IntegerField(null=True)
     biome8_attack_multiplier = models.IntegerField(null=True)
     created = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True, editable=False, null=False, blank=False)
+    last_modified = models.DateTimeField(
+        auto_now=True, editable=False, null=False, blank=False
+    )
 
     class Meta:
         abstract = True
 
+
 class Ally(BaseAlly):
     user_id = models.IntegerField(primary_key=True)
-    owner = models.ForeignKey('self', null=True, related_name='owned_allies', on_delete=models.RESTRICT)
+    owner = models.ForeignKey(
+        "self", null=True, related_name="owned_allies", on_delete=models.RESTRICT
+    )
 
     def historical_records(self):
         return HistoricalAlly.objects.filter(user_id=self.user_id)
 
+
 class HistoricalAlly(BaseAlly):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_id = models.IntegerField(db_index=True)
-    owner = models.ForeignKey(Ally, null=True, related_name='owned_historical_allies', on_delete=models.RESTRICT)
+    owner = models.ForeignKey(
+        Ally,
+        null=True,
+        related_name="owned_historical_allies",
+        on_delete=models.RESTRICT,
+    )
+
 
 class Clan(models.Model):
     id = models.IntegerField(primary_key=True, db_index=True)
