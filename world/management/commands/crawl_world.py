0787da6a59c819636bf01905f57dc0394c716f64
Noam Rathaus <noamr@ssd-disclosure.com>
2022-12-13 11:02:30 +0200

    Styling

--- world/management/commands/crawl_world.py
+++ world/management/commands/crawl_world.py
@@ -1,3 +1,4 @@
+"""Crawl World implementation"""
 from django.core.management.base import BaseCommand
 from world.importer import WorldImporter
 
@@ -5,33 +6,33 @@ from django.conf import settings
 
 
 class Command(BaseCommand):
-    help = 'Crawl through the world using selected token'
+    help = "Crawl through the world using selected token"
 
     def add_arguments(self, parser):
-        parser.add_argument('token',  type=int)
+        parser.add_argument("token", type=int)
 
     def handle(self, *args, **options):
         """
         Usage: python manage.py crawl_world token "2"
         """
         staytoken = settings.STAY_ALIVE_TOKEN
-        if options['token'] == 1:
+        if options["token"] == 1:
             token = settings.HECKFIRE_API_TOKEN
-        elif options['token'] == 128:
+        elif options["token"] == 128:
             token = settings.TOKEN_128
-        elif options['token'] == 106:
+        elif options["token"] == 106:
             token = settings.TOKEN_106
-        elif options['token'] == 10:
+        elif options["token"] == 10:
             token = settings.TOKEN_10
-        elif options['token'] == 92:
+        elif options["token"] == 92:
             token = settings.TOKEN_92
-        elif options['token'] == 99:
+        elif options["token"] == 99:
             token = settings.TOKEN_99
-        elif options['token'] == 129:
+        elif options["token"] == 129:
             token = settings.TOKEN_129
-        elif options['token'] == 121:
+        elif options["token"] == 121:
             token = settings.TOKEN_121
-        elif options['token'] == 130:
+        elif options["token"] == 130:
             token = settings.TOKEN_130
         importer = WorldImporter(token=token, staytoken=staytoken)
         importer.execute(lowerbound=1868, upperbound=6328)
